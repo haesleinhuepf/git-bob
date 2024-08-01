f732c5aad16fdc3b04e0ad8bd0c0bbe5477c8a22
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-08-01 15:50:09 +0200

    WIP: respond to conversation

--- actions/manager.py
+++ actions/manager.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from .github_utilities import comment_on_issue, get_conversation_on_issue
+
 print("Hello")
 # Read the environment variable "ANTHROPIC_API_KEY"
 api_key = os.environ.get("ANTHROPIC_API_KEY")
@@ -16,3 +18,14 @@ if api_key[0] == '"':
 print("Script arguments:")
 for arg in sys.argv[1:]:
     print(arg)
+
+task = sys.argv[1]
+repository = sys.argv[2] if len(sys.argv) > 2 else None
+issue = sys.argv[3] if len(sys.argv) > 3 else None
+
+if task == "response-pull-request":
+
+    comment_on_issue(repository, issue, "Hellooo world!")
+
+print("Conversation:")
+print(get_conversation_on_issue(repository, issue))
