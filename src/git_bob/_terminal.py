f3376947e236e3e93b565c52193a4e531fc69d08
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-08-02 08:37:20 +0200

    only time-out in case we run in the github ci

--- src/git_bob/_terminal.py
+++ src/git_bob/_terminal.py
@@ -7,13 +7,6 @@ def command_line_interface():
     from ._ai_github_utilities import setup_ai_remark, solve_github_issue, review_pull_request, comment_on_issue
     from ._endpoints import prompt_claude, prompt_chatgpt
 
-    def handler(signum, frame):
-        print("Process timed out")
-        sys.exit(1)
-
-    signal.signal(signal.SIGALRM, handler)
-    signal.alarm(180)  # Set the timeout to 3 minutes
-
     print("Hello")
     llm_name = os.environ.get("GIT_BOB_LLM_NAME")
     if "claude" in llm_name and os.environ.get("ANTHROPIC_API_KEY") is not None:
@@ -35,6 +28,15 @@ def command_line_interface():
     # test if we're running in the github-CI
     running_in_github_ci = task.endswith("-action")
     task = task.replace("-action", "")
+
+    if running_in_github_ci:
+        # in case we run in the github-CI, we set a timeout of 3 minutes
+        def handler(signum, frame):
+            print("Process timed out")
+            sys.exit(1)
+
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(180)  # Set the timeout to 3 minutes
 
     repository = sys.argv[2] if len(sys.argv) > 2 else None
     issue = int(sys.argv[3]) if len(sys.argv) > 3 else None
