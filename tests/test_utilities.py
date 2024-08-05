68ecc452f51349ddc4808a955a6fce4c687a9d9c
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-08-05 17:45:51 +0200

    add test

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,11 +1,13 @@
-from git_bob._utilities import remove_indentation
+from git_bob._utilities import remove_indentation, remove_outer_markdown
 
 def test_remove_indentation():
     assert remove_indentation("    Hello") == "Hello"
     assert remove_indentation("        Hello") == "    Hello"
 
-def test_function2():
-    pass
+def test_remove_outer_markdown():
+    assert remove_outer_markdown("""```python
+bla
+```""") == "bla"
 
 def test_function3():
     pass
