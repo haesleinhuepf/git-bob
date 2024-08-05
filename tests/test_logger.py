ac5f2953db09a8e81565f604d060684a9b5600d4
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-08-05 18:17:24 +0200

    test logger as well

---
+++ tests/test_logger.py
@@ -0,0 +1,7 @@
+def test_log():
+    from git_bob._logger import Log
+    log = Log()
+    log.log("Hello")
+    assert log.get() == ["Hello"]
+    log.log("World")
+    assert log.get() == ["Hello", "World"]
