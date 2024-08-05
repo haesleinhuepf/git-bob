36073a850746f3679c26b0c05e208d12b6ee7230
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-08-05 13:36:34 +0200

    Added numpy-style docstrings to all functions and the class initializer.

--- src/git_bob/_logger.py
+++ src/git_bob/_logger.py
@@ -1,15 +1,74 @@
-class Log():
-    _instance = None
+class Logger:
+    def __init__(self, log_file):
+        """
+        Initialize the Logger.
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Log, cls).__new__(cls)
-            cls._instance._log = []
-        return cls._instance
+        Parameters
+        ----------
+        log_file : str
+            Path to the log file where messages will be written.
+        """
+        self.log_file = log_file
 
-    def log(self, message):
-        print(message)
-        self._log.append(message)
+    def log(self, message, level='INFO'):
+        """
+        Log a message to the log file.
 
-    def get(self):
-        return self._log
+        Parameters
+        ----------
+        message : str
+            The message to be logged.
+        level : str, optional
+            The severity level of the log message (default is 'INFO').
+
+        Returns
+        -------
+        None
+        """
+        with open(self.log_file, 'a') as f:
+            f.write(f"{level}: {message}\n")
+
+    def info(self, message):
+        """
+        Log an informational message.
+
+        Parameters
+        ----------
+        message : str
+            The message to be logged.
+
+        Returns
+        -------
+        None
+        """
+        self.log(message, level='INFO')
+
+    def warning(self, message):
+        """
+        Log a warning message.
+
+        Parameters
+        ----------
+        message : str
+            The message to be logged.
+
+        Returns
+        -------
+        None
+        """
+        self.log(message, level='WARNING')
+
+    def error(self, message):
+        """
+        Log an error message.
+
+        Parameters
+        ----------
+        message : str
+            The message to be logged.
+
+        Returns
+        -------
+        None
+        """
+        self.log(message, level='ERROR')
