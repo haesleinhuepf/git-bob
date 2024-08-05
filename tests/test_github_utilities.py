81090e07fc48cbe1847d9eadac7c4b33801f2eb4
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-08-05 17:55:16 +0200

    add more tests

--- tests/test_github_utilities.py
+++ tests/test_github_utilities.py
@@ -1,22 +1,31 @@
-from git_bob._github_utilities import get_github_repository
-
 def test_get_github_repository():
+    from git_bob._github_utilities import get_github_repository
     assert get_github_repository("haesleinhuepf/git-bob").name == "git-bob"
 
 def test_get_github_issue():
-    assert False
+    from git_bob._github_utilities import get_github_issue_details
+    assert "Issue #1: Testing conversational workflows" in get_github_issue_details("haesleinhuepf/git-bob", 1)
 
-def test_get_github_pull_request():
-    pass
+def test_get_conversation_on_issue():
+    from git_bob._github_utilities import get_conversation_on_issue
+    conversation = get_conversation_on_issue("haesleinhuepf/git-bob", 20)
 
-def test_create_github_pull_request():
-    pass
+    assert "What is the captial of France?" in conversation
+    assert "France" in conversation
 
-def test_update_github_pull_request():
-    pass
+def test_get_most_recent_comment_on_issue():
+    from git_bob._github_utilities import get_most_recent_comment_on_issue
+    user, comment = get_most_recent_comment_on_issue("haesleinhuepf/git-bob", 20)
 
-def test_get_github_file_content():
-    pass
+    assert "What is the captial of France?" not in comment
+    assert "France" in comment
+
+def test_list_issues():
+    from git_bob._github_utilities import list_issues
+    closed_issues = list(list_issues("haesleinhuepf/git-bob", state="closed").keys())
+
+    assert 1 in closed_issues
+    assert 20 in closed_issues
 
 def test_update_github_file():
     pass
