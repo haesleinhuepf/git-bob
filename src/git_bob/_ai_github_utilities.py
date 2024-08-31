954dc22250c4ae4eea1cc4932b8bb780fc0f7176
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-08-31 14:59:00 +0200

    prompt engineering

--- src/git_bob/_ai_github_utilities.py
+++ src/git_bob/_ai_github_utilities.py
@@ -436,7 +436,7 @@ The following changes were made in the files:
 ## Your task
 Summarize the changes above to a one paragraph line Github pull-request message. 
 Afterwards, summarize the summary in a single line, which will become the title of the pull-request.
-Do not add headnline or any other formatting. Just respond with the paragraphe and the title in a new line below.
+Do not add headline or any other formatting. Just respond with the paragraphe and the title in a new line below.
 """)
 
     pull_request_description, pull_request_title = split_content_and_summary(pull_request_summary)
@@ -461,11 +461,13 @@ def split_issue_in_sub_issues(repository, issue, prompt_function):
     from ._github_utilities import create_issue, add_comment_to_issue, get_conversation_on_issue
 
     discussion = get_conversation_on_issue(repository, issue)
+    ai_remark = setup_ai_remark()+ "\n"
 
     # Implement the prompt to parse the discussion
     sub_tasks_json = prompt_function(f"""
 {SYSTEM_PROMPT}
 You need to extract sub-tasks from a given discussion.
+Hint: Sub-tasks are never about "Create an issue for X", but "X" instead. Also sub-tasks are never about "Propose X", but "X" instead.
 Return a JSON list with a short title for each sub-task.
 
 ## Discussion
@@ -476,6 +478,7 @@ Extract and return sub-tasks as a JSON list of sub-task titles.
 """)
 
     sub_tasks = text_to_json(sub_tasks_json)
+    created_sub_tasks = ""
 
     sub_issue_numbers = []
     for title in sub_tasks:
@@ -487,21 +490,28 @@ extract relevant information for one of the sub-tasks.
 ## Discussion
 {discussion}
 
+{created_sub_tasks}
+
 ## Your task
 Extract relevant information for the sub-task "{title}".
 Write the information in a short paragraph. Assume this paragraph will be the body of a new issue.
 Hence, all necessary information should be included.
-Do not explain your response or anything else. Just respond the paragraph about the sub-task.
+Also add a paragraph with a first proposal of how to solve the sub-task.
+Do not explain your response or anything else. Just respond the two paragraphs about the sub-task and its potential solution.
 """)
         body = body.replace("git-bob", "git=bob") # prevent endless loops
 
-        issue_number = create_issue(repository, title, body)
+        issue_number = create_issue(repository, title, ai_remark + body)
         sub_issue_numbers.append(issue_number)
-    
+
+        if len(created_sub_tasks) == 0:
+            created_sub_tasks = "## Other sub-tasks\nThe following sub-tasks have already been identified:\n"
+        created_sub_tasks += f"### {title}\n{body}\n\n"
+
     # Create a comment on the main issue with the list of sub-issues
     sub_issue_links = "\n".join([f"- #{num}" for num in sub_issue_numbers])
     comment_text = f"Sub-issues have been created:\n{sub_issue_links}"
-    add_comment_to_issue(repository, issue, comment_text)
+    add_comment_to_issue(repository, issue, ai_remark + comment_text)
 
     return sub_issue_numbers
 
