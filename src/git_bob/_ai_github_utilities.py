03253c4f48e91b6f08e7e917a7aa7d662f5a969e
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-10-30 20:35:13 +0100

    tune number of sentences per slides

--- src/git_bob/_ai_github_utilities.py
+++ src/git_bob/_ai_github_utilities.py
@@ -257,7 +257,7 @@ def create_or_modify_file(repository, issue, filename, branch_name, issue_summar
 The file should be a presentation with slides, formatted as a JSON list containing dictionaries with a 'title' and a 'content' list with up to 2 strings. 
 These strings can be text+text or text+image. The strings can be multi-line text, and also be file-paths of .jpg, .gif or .png files. 
 If it's an image, it MUST only be the file-path and no additional text.
-If it's text, make sure the text is short enough that it fits on a slide. Consider using bullet-points instead of long sentences.
+If it's text, make sure the text is short enough that it fits on a slide. Also put enough information on a slide so that it doesn't appear empty. Two to four sentences per slide are nice. For more detailed information consider using bullet-points instead of long sentences. Four to six bullet points per slide are great. 
 Choose from these existing files and only use them if they fit well to the content:\n* """ + \
                 "\n* ".join(Config.git_utilities.list_repository_files(repository, branch_name=branch_name, file_patterns=image_file_endings)) + "\n\n"
 
