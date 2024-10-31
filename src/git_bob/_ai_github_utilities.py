bbbe1766150cdf69d70c2866dfaf93b8209b970d
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-10-31 08:44:54 +0100

    add example to prompt

--- src/git_bob/_ai_github_utilities.py
+++ src/git_bob/_ai_github_utilities.py
@@ -254,12 +254,16 @@ def create_or_modify_file(repository, issue, filename, branch_name, issue_summar
             format_specific_instructions = " In the notebook file, write short code snippets in code cells and avoid long code blocks. Make sure everything is done step-by-step and we can inspect intermediate results. Add explanatory markdown cells in front of every code cell. The notebook has NO cell outputs! Make sure that there is code that saves results such as plots, images or dataframes, e.g. as .png or .csv files. Numpy images have to be converted to np.uint8 before saving as .png. Plots must be saved to disk before the cell ends or it is shown. The notebook must be executable from top to bottom without errors. Return the notebook in JSON format!"
         elif filename.endswith('.pptx'):
             format_specific_instructions = """
-The file should be a presentation with slides, formatted as a JSON list containing dictionaries with a 'title' and a 'content' list with up to 2 strings. 
+The file should be a presentation with slides, formatted as a JSON list containing dictionaries with a 'title' and a 'content' list with up to 2 strings.
 These strings can be text+text or text+image. The strings can be multi-line text, and also be file-paths of .jpg, .gif or .png files. 
 If it's an image, it MUST only be the file-path and no additional text.
-If it's text, make sure the text is short enough that it fits on a slide. Also put enough information on a slide so that it doesn't appear empty. Two to four sentences per slide are nice. For more detailed information consider using bullet-points instead of long sentences. Four to six bullet points per slide are great. 
+If it's text, make sure the text is short enough that it fits on a slide. Also put enough information on a slide so that it doesn't appear empty. Two to four sentences per slide are nice. For more detailed information consider using bullet-points instead of long sentences. Four to six bullet points per slide are great.
 Choose from these existing files and only use them if they fit well to the content:\n* """ + \
-                "\n* ".join(Config.git_utilities.list_repository_files(repository, branch_name=branch_name, file_patterns=image_file_endings)) + "\n\n"
+                "\n* ".join(Config.git_utilities.list_repository_files(repository, branch_name=branch_name, file_patterns=image_file_endings)) + "\n\n" + \
+                """
+Example 1: {"title":"headline", "content":["placeholder text A", "placeholder text A"]}
+Example 2: {"title":"topic", "content":["placeholder text", "placeholder_image.png"]}
+"""
 
         file_content = None
         if Config.git_utilities.check_if_file_exists(repository, branch_name, filename):
