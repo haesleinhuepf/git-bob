cf2dc3d62204baa5c4a1f22602b7c783b54ce28c
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-12-15 15:27:36 +0100

    debugging

--- src/git_bob/_github_utilities.py
+++ src/git_bob/_github_utilities.py
@@ -363,8 +363,10 @@ def write_file_in_branch(repository, branch_name, file_path, new_content, commit
     # Commit the changes
     if check_if_file_exists(repository, branch_name, file_path):
         file = get_file_in_repository(repository, branch_name, file_path)
+        print("update file", file_path, file.sha)
         repo.update_file(file.path, commit_message, new_content, file.sha, branch=branch_name)
     else:
+        print("create file", file_path)
         repo.create_file(file_path, commit_message, new_content, branch=branch_name)
 
     # ensure the folder extists
