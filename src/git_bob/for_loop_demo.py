dd781f36ea6cf13dfc34f9b2d2bfd850f71449e2
Robert Haase <haesleinhuepf@users.noreply.github.com>
2025-01-25 15:16:45 +0100

    Created a Python script that demonstrates a for-loop outputting numbers from 0 to 10.

---
+++ src/git_bob/for_loop_demo.py
@@ -0,0 +1,11 @@
+def print_numbers():
+    """
+    Prints numbers from 0 to 10.
+
+    Demonstrates the use of a for-loop.
+    """
+    for i in range(11):
+        print(i)
+
+if __name__ == "__main__":
+    print_numbers()
