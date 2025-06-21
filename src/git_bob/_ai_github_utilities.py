c28bd20a51c2e8eb8a1156fbfc2d18c68478e1d2
Robert Haase <haesleinhuepf@users.noreply.github.com>
2025-06-21 23:19:20 +0200

    tell the file modifier which other files exist

--- src/git_bob/_ai_github_utilities.py
+++ src/git_bob/_ai_github_utilities.py
@@ -310,6 +310,7 @@ def create_or_modify_file(repository, issue, filename, branch_name, issue_summar
     if is_ignored(filename, repository, branch_name):
         raise ValueError(f"Access to {filename} is restricted by .gitbobignore")
 
+    more_instructions = ""
     created_files = {}
     for attempt in range(number_of_attempts):
         an_error_happened = False
@@ -317,14 +318,23 @@ def create_or_modify_file(repository, issue, filename, branch_name, issue_summar
         created_files = {}
         original_ipynb_file_content = None
 
+        all_files = """
+## Other files in the repository
+
+You are working on file {filename}. When you are writing code that accesses other files, make sure to use the correct relative file-paths.
+These are all files in the repository:
+""" + "* " + "\n* ".join(Config.git_utilities.list_repository_files(repository, branch_name=branch_name))
+
         format_specific_instructions = ""
         if any([filename.endswith(f) for f in image_file_endings]):
             print(f"Cannot create or modify image files like {filename}.")
             return created_files
         elif filename.endswith('.py'):
             format_specific_instructions = " When writing new functions, use numpy-style docstrings."
-        elif filename.endswith('.ipynb'):
+            more_instructions = all_files
+        if filename.endswith('.ipynb'):
             format_specific_instructions = " In the notebook file, write short code snippets in code cells and avoid long code blocks. Make sure everything is done step-by-step and we can inspect intermediate results. Add explanatory markdown cells in front of every code cell. The notebook has NO cell outputs! Make sure that there is code that saves results such as plots, images or dataframes, e.g. as .png or .csv files. Numpy images have to be converted to np.uint8 before saving as .png. Plots must be saved to disk before the cell ends or it is shown. The notebook must be executable from top to bottom without errors. Return the notebook in JSON format!"
+            more_instructions = all_files
         elif filename.endswith('.docx'):
             format_specific_instructions = " Write the document in simple markdown format."
         elif filename.endswith('.mp3'):
@@ -370,6 +380,7 @@ Example 5: {"title":"Summary", "content":["In this slide-deck we learned about\n
 Modify the file "{filename}" to solve the issue #{issue}. {format_specific_instructions}
 If the discussion is long, some stuff might be already done. In that case, focus on what was said at the very end in the discussion.
 Keep your modifications absolutely minimal.
+{more_instructions}
 
 That's the file "{filename}" content you will find in the file:
 ```
